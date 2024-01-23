58265289bfd3c523687be6a5ad4a53ec65729721
Giri292002 <pbgiridhara@gmail.com>
2024-01-23 12:47:51 -0800

    Initial Commit

---
+++ ProjectLaugh/Source/ProjectLaugh/ProjectLaughGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectLaughGameMode.h"
+#include "ProjectLaughCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AProjectLaughGameMode::AProjectLaughGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ ProjectLaugh/Source/ProjectLaugh/ProjectLaughGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectLaughGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectLaughGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectLaughGameMode();
+};
+
+
+
